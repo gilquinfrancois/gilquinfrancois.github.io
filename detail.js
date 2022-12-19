fa11e7d2e9c7c38e343e1fedb4336e75e93d3a21
francois gilquin <francoisgilquin@yahoo.fr>
2022-12-19 21:47:35 +0100

    modif menu dim + modif text

--- detail.js
+++ detail.js
@@ -1,13 +1,19 @@
 $(document).ready(function(){
 
-
+    $(".buttonPlusAp").click(function(){
+        $(".detail").css("display","block");
+        $(".detail").animate({height:'60%'},"slow");
+        $(".buttonPlus").css("z-index",0);
+        $('#titreDetail').text("Aperitif");
+        $('#textDetail').html("<h5>Verrines de cappuccino glacé carottes et cumin</h5><h5>Verrines apéro de velouté d'avocat</h5><h5>Mini-verrines apéritives saumon, crevettes et oeufs de poisson</h5>");
+    });
 
     $(".buttonPlusEF").click(function(){
         $(".detail").css("display","block");
         $(".detail").animate({height:'60%'},"slow");
         $(".buttonPlus").css("z-index",0);
         $('#titreDetail').text("Entree Froide");
-        $('#textDetail').text("Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint id cumque nemo ratione veniam possimus explicabo ullam tenetur enim voluptas sunt at, odit officiis non in animi esse ipsum eius.");
+        $('#textDetail').html("<h5>Foie gras, pains d'épices, framboises et et ces groseilles rouges</h5>");
     });
 
     
@@ -16,7 +22,7 @@ $(document).ready(function(){
         $(".detail").animate({height:'60%'},"slow");
         $(".buttonPlus").css("z-index",0);
         $('#titreDetail').text("Entree Chaude");
-        $('#textDetail').text("Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint id cumque nemo ratione veniam possimus explicabo ullam tenetur enim voluptas sunt at, odit officiis non in animi esse ipsum eius.");
+        $('#textDetail').html("<h5>Trios de poissons</h5>");
     });
 
     $(".buttonPlusS").click(function(){
@@ -24,7 +30,7 @@ $(document).ready(function(){
         $(".detail").animate({height:'60%'},"slow");
         $(".buttonPlus").css("z-index",0);
         $('#titreDetail').text("Soupe");
-        $('#textDetail').text("Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint id cumque nemo ratione veniam possimus explicabo ullam tenetur enim voluptas sunt at, odit officiis non in animi esse ipsum eius.");
+        $('#textDetail').html("<h5>Soupe de potirons</h5>");
     });
 
     $(".buttonPlusP").click(function(){
@@ -32,7 +38,7 @@ $(document).ready(function(){
         $(".detail").animate({height:'60%'},"slow");
         $(".buttonPlus").css("z-index",0);
         $('#titreDetail').text("Plat");
-        $('#textDetail').text("Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint id cumque nemo ratione veniam possimus explicabo ullam tenetur enim voluptas sunt at, odit officiis non in animi esse ipsum eius.");
+        $('#textDetail').html("<h5>Dinde, sauce chasseur avec ces croquettes de Noel et ca pomme aux airelles</h5>");
     });
 
     $(".buttonPlusD").click(function(){
@@ -40,7 +46,7 @@ $(document).ready(function(){
         $(".detail").animate({height:'60%'},"slow");
         $(".buttonPlus").css("z-index",0);
         $('#titreDetail').text("Desert");
-        $('#textDetail').text("Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint id cumque nemo ratione veniam possimus explicabo ullam tenetur enim voluptas sunt at, odit officiis non in animi esse ipsum eius.");
+        $('#textDetail').html("<h5>Buche à la vanille et pralinée</h5>");
     });
 
     $("#croix").click(function(){
